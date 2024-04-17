031477a37528aad6362ddf22151c360b297d094d
Ayush Aditya <127651481+Sayuksh@users.noreply.github.com>
2024-04-17 23:39:12 +0530

    model evalution added

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from textSummarizer.components import data_ingestion
+from textSummarizer.components import data_ingestion, model_evalution, model_trainer
 from textSummarizer.pipline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipline.stage_02_data_validation import DataValidationPipeline
 from textSummarizer.pipline.stage_03_data_transformation import DataTransformationPipeline
+from textSummarizer.pipline.stage_04_model_trainer import ModelTrainerPipeline
+from textSummarizer.pipline.stage_05_model_evalution import ModelEvalutionPipeline
 from textSummarizer.logging import logger
 
 STAGE_NAME="Data ingestion stage"
@@ -28,6 +30,25 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     data_validation=DataTransformationPipeline()
     data_validation.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<< \n\n x================x")
+except Exception as e:
+    raise e
+
+STAGE_NAME="Model Trainer stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_trainer=ModelTrainerPipeline()
+    model_trainer.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<< \n\n x================x")
+except Exception as e:
+    raise e
+
+
+STAGE_NAME="Model Evalution stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_evalution=ModelEvalutionPipeline()
+    model_evalution.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<< \n\n x================x")
 except Exception as e:
     raise e
