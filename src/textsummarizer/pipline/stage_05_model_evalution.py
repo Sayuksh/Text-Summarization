6a0e18d600de01e53178e8acbad16a86cebbf649
Ayush Aditya <127651481+Sayuksh@users.noreply.github.com>
2024-04-17 23:20:33 +0530

    model evalution added

---
+++ src/textsummarizer/pipline/stage_05_model_evalution.py
@@ -0,0 +1,13 @@
+from textSummarizer.config.configuration import ConfigurationManager
+from textSummarizer.components.model_evalution import ModelEvaluation
+from textSummarizer.logging import logger
+
+
+class ModelEvalutionPipeline:
+    def __init__(self):
+        pass
+    def main(self):
+        config = ConfigurationManager()
+        model_evaluation_config = config.get_model_evaluation_config()
+        model_evaluation_config = ModelEvaluation(config=model_evaluation_config)
+        model_evaluation_config.evaluate()
